ef0572a026e080340d8d2337a859b3386e022023
ErfanNikbakhsh <erfan.nikbakhsh1@gmail.com>
2024-05-12 11:02:49 +0330

    ADD newUserRequirements & userLoginRequirements to auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
-const { createUser, userLogin } = require('../middlewares/myMediaBoard/userMiddleware');
+const {
+  createUser,
+  userLogin,
+  newUserRequirements,
+  userLoginRequirements,
+} = require('../middlewares/myMediaBoard/userMiddleware');
 const { sendMessage } = require('../controllers/messageController');
 
 const router = express.Router();
 
-router.post('/register', createUser, sendMessage);
+router.post('/register', newUserRequirements, createUser, sendMessage);
 
-router.post('/login', userLogin, sendMessage);
+router.post('/login', userLoginRequirements, userLogin, sendMessage);
 
 module.exports = router;
